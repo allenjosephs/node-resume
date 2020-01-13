ebaf7520c15f52aa3c1ac3af0511d2ed67bd1b35
Joe Allen <allenjosephs@johndeere.com>
2020-01-13 11:06:22 -0700

    fixed copy paste error

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 #! /usr/bin/env node
 
-fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
+const fs = require("fs");
+
+fs.readFile(__dirname + "/info.json", "utf8", function(err, data) {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
-    console.log(data)
-    return data
+    console.log(data);
+    return data;
   }
-})
+});
